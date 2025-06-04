5919f8b4564b63634ae423b64163189b2c8555d8
ravi.chand <chandravi00123@gmail.com>
2025-06-04 10:11:02 +0000

    all deleted

--- dev-subscription-ms-testing/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "subscription-testing-terraform-tf"
-    key = "ecs/subscription/terraform.tfstate"
-    region = "us-west-2"
-    encrypt = true
-    dynamodb_table = "subscription-terraform-locking"
-  }
-}
--- dev-subscription-ms-testing/modules/ecr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repo_uris" {
-  description = "Map of repository names to their URIs"
-  value = {
-    for repo_name, repo in aws_ecr_repository.this :
-    repo_name => repo.repository_url
-  }
-}
